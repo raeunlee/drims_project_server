c71588dab6839afdb95483490ae6ed509906da86
chul <yk2915@naver.com>
2021-12-15 15:49:05 +0900

    1

--- app.js
+++ app.js
@@ -1,13 +1,79 @@
-const express = require("express");
-const app = express();
+const express=require('express');
+const mysql=require('mysql');
 const cors=require('cors');
+const app=express();
+const axios=require('axios');
+
+const user=require('./routes/user');
+
+app.use('/user',user);
+
 app.use(cors());
+app.use(express.json());
 
-app.get("/hi", function(req,res) {
-	res.send("Welcome mini_proj web");
+
+//create connection
+/*
+const db=mysql.createConnection({
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'2915',
+    database: 'tripbudget'
+});
+*/
+
+const db=mysql.createConnection({
+  host: "travel-project.clzbzgdwjz4i.ap-northeast-2.rds.amazonaws.com",
+  user: "root",
+  password: "drims2021!",
+  database: "travelbudget"
+});
+//conect
+db.connect((err)=>{
+    if(err){
+        throw err;
+    }
+    console.log('mysql connected');
+})
+
+/*
+db.query('INSERT INTO user (id,name,birth,email,password) VALUES(?,?,?,?,?)',
+['chul0129','윤철','960129','jason@drimaes.com','1234'],
+(err,result)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log('Values Inserted');
+    }
+    
+}
+);
+*/
+app.post('/create', (req,res)=>{
+    const id=req.body.id
+    const name=req.body.name
+    const date_start=req.body.date_start
+    const date_end=req.body.date_end
+    const money=req.body.money  
+    
+    db.query('INSERT INTO travel (id,name,date_start,date_end,money) VALUES(?,?,?,?,?)',
+    [id,name,date_start,date_end,money],
+    (err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send('Values Inserted');
+        }
+        
+    }
+    );
+
 });
 
-const PORT = 10000;
-app.listen(PORT, function() {
-    console.log("Server is ready at" + PORT);
-});
+
+
+app.listen('3000',()=>{
+    console.log('server start on port 3000');
+    
+})
