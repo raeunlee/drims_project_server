38426a22dcf1615d3d633d3d432d78895a0ad757
chul <yk2915@naver.com>
2021-12-16 11:51:32 +0900

    '2'

---
+++ routes/country.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const router=express.Router();
+const cors=require('cors');
+router.use(cors());
+router.use(express.json());
+
+router.post("/",(req,res)=>{
+
+    console.log(req.body.text);
+});
+
+router.get('/',(req,res)=>{
+    res.send('user 화면');
+});
+
+module.exports=router;
