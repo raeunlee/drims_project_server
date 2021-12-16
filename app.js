fb759705cb51dbacdca4c50d4a187ced28de657d
raeunlee <raeun19@naver.com>
2021-12-16 11:52:40 +0900

    node_server

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ const app=express();
 const axios=require('axios');
 
 const user=require('./routes/user');
+const res = require('express/lib/response');
 
 app.use('/user',user);
 
 app.use(cors());
 app.use(express.json());
 
+app.get("/hi", function(req,res) {
+        res.send("hihi");
+});
 
 //create connection
 /*
@@ -71,7 +75,15 @@ app.post('/create', (req,res)=>{
 
 });
 
-
+app.get('/travel', (req,res) => {
+    db.query("SELECT * FROM travel", (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+            })
+})
 
 app.listen('3000',()=>{
     console.log('server start on port 3000');
